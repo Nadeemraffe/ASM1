3fde3d4af1a5009bc621a3beb97e6236b212de53
Nadeemraffe <123829445+Nadeemraffe@users.noreply.github.com>
2023-02-02 05:19:03 -0800

    Add files via upload

---
+++ JAVA/app.js
@@ -0,0 +1,8 @@
+// Question No 1
+alert("Error!Please inter a valid password")
+// Question No 2
+alert("Welcom to js land...\nHappy Coding")
+// Question No 3
+alert("Welcom to js land...")
+// Question No 4
+alert("hello..I can run js through to my web broswer's consol")
